a514a9481538b9c45446abd43b661a5710588fe1
JohnyDevp <holan.jan@outlook.com>
2026-04-21 11:07:34 +0200

    fix: some changes with definitions and removing header file

--- src/onefile/benchmark.c
+++ src/onefile/benchmark.c
@@ -3,37 +3,36 @@
 
 #define W LED_MATRIX_0_WIDTH
 #define H LED_MATRIX_0_HEIGHT
-#define W 128
-#define H 64
+
+// *****************************************************************************
+// * LED_MATRIX_0
+// *****************************************************************************
+#define LED_MATRIX_0_BASE (0xf0000000)
+#define LED_MATRIX_0_SIZE (0x8000)
+#define LED_MATRIX_0_WIDTH (0x80)
+#define LED_MATRIX_0_HEIGHT (0x40)
 
 // *****************************************************************************
 // * SWITCHES_0
 // *****************************************************************************
-#define SWITCHES_0_BASE (0xf0000000)
+#define SWITCHES_0_BASE (0xf0008000)
 #define SWITCHES_0_SIZE (0x4)
 #define SWITCHES_0_N (0x8)
 
 // *****************************************************************************
 // * D_PAD_0
 // *****************************************************************************
-#define D_PAD_0_BASE (0xf0000004)
+
+#define D_PAD_0_BASE (0xf0008004)
 #define D_PAD_0_SIZE (0x10)
 #define D_PAD_0_UP_OFFSET (0x0)
-#define D_PAD_0_UP (0xf0000004)
+#define D_PAD_0_UP (0xf0008004)
 #define D_PAD_0_DOWN_OFFSET (0x4)
-#define D_PAD_0_DOWN (0xf0000008)
+#define D_PAD_0_DOWN (0xf0008008)
 #define D_PAD_0_LEFT_OFFSET (0x8)
-#define D_PAD_0_LEFT (0xf000000c)
+#define D_PAD_0_LEFT (0xf000800c)
 #define D_PAD_0_RIGHT_OFFSET (0xc)
-#define D_PAD_0_RIGHT (0xf0000010)
-
-// *****************************************************************************
-// * LED_MATRIX_0
-// *****************************************************************************
-#define LED_MATRIX_0_BASE (0xf0000014)
-#define LED_MATRIX_0_SIZE (0x8000)
-#define LED_MATRIX_0_WIDTH (0x80)
-#define LED_MATRIX_0_HEIGHT (0x40)
+#define D_PAD_0_RIGHT (0xf0008010)
 
 // ******************************************************************************
 // inlucded functions
